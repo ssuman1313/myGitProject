a953444d69d2764dd8ce953588c21035a238e096
Sourav Suman <ssuman1313@gmail.com>
2019-08-02 14:29:38 +0530

    Test

---
+++ seleniumPOC/src/com/facebook/reachFacebook/AllTests.java
@@ -0,0 +1,11 @@
+package com.facebook.reachFacebook;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ facebookLoginPage.class })
+public class AllTests {
+
+}
---
+++ seleniumPOC/src/com/facebook/reachFacebook/facebookLoginPage.java
@@ -0,0 +1,39 @@
+package com.facebook.reachFacebook;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+
+@SuppressWarnings("unused")
+public class facebookLoginPage {
+
+	private WebDriver driver;
+
+	@After
+	public void tearDown() {
+		driver.quit();
+	}
+
+	@Before
+	public void setUp() {
+		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+	}
+
+	@Test
+	public void OpenFB() {
+
+		driver.get("https://www.facebook.com");
+	}
+
+	@Test
+	public void OpenGmail() {
+		driver.get("https://www.gmail.com");
+	}
+
+}
